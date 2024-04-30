f49ff9e103683dfa739fbdc7680819a43a4ea225
Taehoon Kim <taehoon.kim@9folders.com>
2024-04-30 11:55:48 +0900

    [*] 마무으리

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import json
 import os
 import subprocess
 import calendar
+from collections import defaultdict
+import re
+
 
 dev_bastion_host = "3.38.97.146"
 dev_bastion_user = "ubuntu"
@@ -56,6 +59,106 @@ m = [
 year = datetime.now().year
 last_month = datetime.now().month - 1
 format_month = "{:02}".format(last_month)
+
+
+def load_db_data():
+    with SSHTunnelForwarder(
+        (bastion_host, 22),
+        ssh_username=bastion_user,
+        ssh_pkey=bastion_key,
+        remote_bind_address=(
+            rds_read_host,
+            3306,
+        ),
+    ) as tunnel:
+        conn = pymysql.connect(
+            host="localhost",
+            port=tunnel.local_bind_port,
+            user=rds_user,
+            passwd=rds_password,
+            db="company_logo",
+        )
+
+        # tunnel.start()
+
+        # global domain_list
+        # domain_list = []
+        try:
+            with conn.cursor() as cur:
+                # cur.execute("show databases")
+
+                cur.execute("select domain from company_logo;")
+                for r in cur:
+                    # print(r[0])
+                    domain_list.append(r[0])
+            # print(domain_list)
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            cur.close()
+            print("load_data OK")
+        # print(json.dumps(domain_list, indent=2, ensure_ascii=False), len(domain_list))
+
+    #########################################
+
+    # 1. os.system("ls -la > xxx.log")
+    # with open("xxx.log", "r") as fr:
+    # buf = fr.read()
+
+    # str_cmd = "ls -la > xxx.log"
+    # 2. subprocess.open(str_cmd.split())
+
+
+def log_contrast_with_db():
+    with open(
+        f"/Users/taehoon/logs/daily_top_request_domain_{file_date}.log", "r"
+    ) as f:
+        lines = f.readlines()
+        for line in lines:
+            first_domain = line.split()[1].replace("\n", "")
+            parse_domain = extract(first_domain, include_psl_private_domains=False)
+
+            # is_subdomain = parse_domain.subdomain
+
+            domain_without_subdomain = parse_domain.registered_domain
+            domain_without_subdomain_count = int(line.split()[0])
+
+            if domain_without_subdomain == "":
+                break
+
+            log_domain_list.append(
+                [int(domain_without_subdomain_count), domain_without_subdomain]
+            )
+
+        final_log_domain_list.append([0, 0])
+
+        # 로그 안에 있는 동일 도메인 하나로 합침
+        for log_domain_item in log_domain_list:
+            count = 0
+            for final_domain_item in final_log_domain_list:
+                if log_domain_item[1] in final_domain_item:
+                    count += 1
+                    final_domain_item[0] += log_domain_item[0]
+            if count == 0:
+                final_log_domain_list.append([log_domain_item[0], log_domain_item[1]])
+        # DB 에 있는 도메인
+        for final_list_item in final_log_domain_list:
+            count = 0
+            for domain_list_item in domain_list:
+                if final_list_item[1] == domain_list_item:
+                    count += 1
+                    break
+            if count == 0:
+                uniq_list.append(final_list_item)
+
+        # uniq_list.pop(0)
+        uniq_list.sort(reverse=True)
+
+        with open(f"/Users/taehoon/logs/no_filter_result_{file_date}.log", "a") as ff:
+            for i in uniq_list:
+                ff.write(f"{i}\n")
+    print("log_contrast_with_db OK")
+    delete_file(f"/Users/taehoon/logs/daily_top_request_domain_{file_date}.log")
 
 
 def input_deactivate_data():
@@ -153,144 +256,6 @@ def input_deactivate_data():
             cur.close()
 
 
-def load_db_data():
-    with SSHTunnelForwarder(
-        (bastion_host, 22),
-        ssh_username=bastion_user,
-        ssh_pkey=bastion_key,
-        remote_bind_address=(
-            rds_read_host,
-            3306,
-        ),
-    ) as tunnel:
-        conn = pymysql.connect(
-            host="localhost",
-            port=tunnel.local_bind_port,
-            user=rds_user,
-            passwd=rds_password,
-            db="company_logo",
-        )
-
-        # tunnel.start()
-
-        # global domain_list
-        # domain_list = []
-        try:
-            with conn.cursor() as cur:
-                # cur.execute("show databases")
-
-                cur.execute("select domain from company_logo;")
-                for r in cur:
-                    # print(r[0])
-                    domain_list.append(r[0])
-            # print(domain_list)
-        except Exception as e:
-            print(f"Error: {e}")
-        finally:
-            cur.close()
-            print("load_data OK")
-        # print(json.dumps(domain_list, indent=2, ensure_ascii=False), len(domain_list))
-
-    #########################################
-
-    # 1. os.system("ls -la > xxx.log")
-    # with open("xxx.log", "r") as fr:
-    # buf = fr.read()
-
-    # str_cmd = "ls -la > xxx.log"
-    # 2. subprocess.open(str_cmd.split())
-
-
-def log_contrast_with_db():
-    with open(
-        f"/Users/taehoon/logs/daily_top_request_domain_{file_date}.log", "r"
-    ) as f:
-        a = f.readlines()
-        for i in a:
-            first_domain = i.split()[1].replace("\n", "")
-            parse_domain = extract(first_domain, include_psl_private_domains=False)
-
-            is_subdomain = parse_domain.subdomain
-
-            domain_without_subdomain = parse_domain.registered_domain
-            domain_without_subdomain_count = int(i.split()[0])
-            if domain_without_subdomain == "":
-                continue
-            log_domain_list.append(
-                [int(domain_without_subdomain_count), domain_without_subdomain]
-            )
-        final_log_domain_list.append([0, 0])
-        for test in log_domain_list:
-            count = 0
-            for test2 in final_log_domain_list:
-                if test[1] in test2:
-                    count += 1
-                    test2[0] += test[0]
-            if count == 0:
-                final_log_domain_list.append([test[0], test[1]])
-        del final_log_domain_list[-1]
-        # print(json.dumps(final_log_domain_list))
-
-        for i in final_log_domain_list:
-            count = 0
-            # print(i)
-            for j in domain_list:
-                if i[1] == j:
-                    count += 1
-                    break
-            if count == 0:
-                uniq_list.append(i)
-
-        # uniq_list.pop(0)
-        uniq_list.sort(reverse=True)
-
-        with open(f"/Users/taehoon/logs/no_filter_result_{file_date}.log", "a") as ff:
-            for i in uniq_list:
-                ff.write(f"{i}\n")
-    print("log_contrast_with_db OK")
-    delete_file(f"/Users/taehoon/logs/daily_top_request_domain_{file_date}.log")
-
-
-# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-# 운영 서버 db에 넣기
-def input_data():
-    with SSHTunnelForwarder(
-        (bastion_host, 22),
-        ssh_username=bastion_user,
-        ssh_pkey=bastion_key,
-        remote_bind_address=(
-            rds_write_host,
-            3306,
-        ),
-    ) as tunnel:
-        conn = pymysql.connect(
-            host="localhost",
-            port=tunnel.local_bind_port,
-            user=rds_user,
-            passwd=rds_password,
-            db="company_logo",
-        )
-
-        # tunnel.start()
-
-        # global domain_list
-        # domain_list = []
-        try:
-            with conn.cursor() as cur:
-                # cur.execute("show databases")
-                cur.execute(
-                    "insert into company_logo (domain, title, hash) VALUES ('mango.com', 'mango', '111');"
-                )
-                conn.commit()
-            # print(domain_list)
-        except Exception as e:
-            print(f"Error: {e}")
-        finally:
-            cur.close()
-
-        # print(json.dumps(domain_list, indent=2, ensure_ascii=False), len(domain_list))
-
-
 def without_one_five_log():
     with open(f"/Users/taehoon/logs/no_filter_result_{file_date}.log") as f:
         a = f.readlines()
@@ -332,9 +297,6 @@ def sum_log():
 
 
 def sum_log_sort():
-    from collections import defaultdict
-    import re
-
     domain_pattern = re.compile(r"^[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$")
 
     with open(f"/Users/taehoon/logs/{m[datetime.now().month-1]}_log_sum.log", "r") as f:
@@ -348,6 +310,7 @@ def sum_log_sort():
                 temprory_domain_list.append([count, domain])
             else:
                 pass
+
         domain_counts = defaultdict(int)
         for count, domain in temprory_domain_list:
             domain_counts[domain] += int(count)
@@ -366,21 +329,23 @@ def sum_log_sort():
 # 가공된 기본 로그 생성 -> daily_top_request_domain.log 파일 생성
 command = "sh /Users/taehoon/make_log_file.sh"
 os.system(command)
-#
-#
-# # DB에서 데이터 불러옴
+
+
+# DB에서 데이터 불러옴
 load_db_data()
-#
-#
-# # 규격화된 로그 DB 파일로 생성 -> no_filter_result.log 파일 생성 및 daily_top_request_domain.log 파일 삭제
+
+
+# 규격화된 로그 DB 파일로 생성 -> no_filter_result.log 파일 생성 및 daily_top_request_domain.log 파일 삭제
 log_contrast_with_db()
 #
 #
-# # 5이상의 도메인만 로그 파일로 다시 생성;; -> result.log 파일 생성 및 no_filter_result.log 파일 삭제
+# # # 5이상의 도메인만 로그 파일로 다시 생성;; -> result.log 파일 생성 및 no_filter_result.log 파일 삭제
 without_one_five_log()
-
-
-if datetime.now().day == 1:
+#
+if (
+    datetime.now().day
+    == calendar.monthrange(datetime.now().year, datetime.now().month)[1]
+):
     # 한달치 로그 합침 -> monthly_log_sum.log 파일 생성
     sum_log()
     #
